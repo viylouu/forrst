55c60fce30510adba43548f3b88447ef1acbb8fc
viylouu <131220600+viylouu@users.noreply.github.com>
2025-09-30 19:04:11 -0600

    just to be clear, that last message said "macRos", not "macos"

--- core/macros.h
+++ core/macros.h
@@ -3,15 +3,18 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 
-typedef signed char s8;
-typedef short s16;
-typedef int s32;
-typedef long long s64;
-typedef unsigned char u8;
-typedef unsigned short u16;
-typedef unsigned int u32;
-typedef unsigned long long u64;
+typedef int8_t s8;
+typedef int16_t s16;
+typedef int32_t s32;
+typedef int64_t s64;
+typedef uint8_t u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+typedef intptr_t sptr;
+typedef uintptr_t uptr;
 typedef float f32;
 typedef double f64;
 typedef u8 b8;
