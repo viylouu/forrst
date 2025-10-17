adbd58ac0d6b96c78fd70934eee05d7a87b374d7
viylouu <131220600+viylouu@users.noreply.github.com>
2025-10-17 16:02:13 -0600

    todo: do stuff

---
+++ core/data/shader.h
@@ -0,0 +1,4 @@
+#ifndef FUR_SHADER_H
+#define FUR_SHADER_H
+
+#endif
---
+++ core/data/texture.h
@@ -0,0 +1,17 @@
+#ifndef FUR_TEXTURE_H
+#define FUR_TEXTURE_H
+
+/* ====== DATATYPES ====== */
+
+    /*
+     */
+typedef void FUR_apiTexture;
+
+    /*
+     *
+     */
+typedef struct {
+    FUR_apiTexture* spec;
+} FUR_texture;
+
+#endif
