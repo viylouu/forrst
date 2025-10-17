586669881f083b329fc5e316c7bae27aac94b95f
viylouu <131220600+viylouu@users.noreply.github.com>
2025-10-17 14:29:03 -0600

    clearer

--- examples/window/main.c
+++ examples/window/main.c
@@ -1,12 +1,10 @@
 #include <platf/agnostic.h>
 #include <render/agnostic.h>
-#include <core/macros.h>
-#include <stdlib.h>
 #include <render/gl/loader.h>
 
 int main(void) {
     FUR_platfState* platf = fur_platf_constr();
-    FUR_renderState* render = fur_render_constr();
+    FUR_renderState* render = fur_render_constr(.api = FUR_RENDER_API_GL);
 
     while (!fur_platf_shouldWindowClose(platf)) {
         fur_platf_poll(platf);
