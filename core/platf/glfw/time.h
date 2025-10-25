c7c9f450a81074d81a6efa8071ad378cc7eb194f
viylouu <131220600+viylouu@users.noreply.github.com>
2025-10-25 12:58:40 -0600

    a

---
+++ core/platf/glfw/time.h
@@ -0,0 +1,6 @@
+#ifndef FUR_PLATFORM_GLFW_TIME_H
+#define FUR_PLATFORM_GLFW_TIME_H
+
+typedef struct { int val; } funnyThingFromEarlier;
+
+#endif
---
+++ core/time.h
@@ -0,0 +1,8 @@
+#ifndef FUR_TIME_H
+#define FUR_TIME_H
+
+typedef struct {
+    int val;
+} FUR_timer;
+
+#endif
